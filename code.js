b80040d696a9c497e55e1d5815b01634862bd8ba
MoniqueDampies <moniquedampies1861@gmail.com>
2022-06-15 16:03:08 +0200

    first

---
+++ code.js
@@ -0,0 +1,9 @@
+var myArray = [1, 5, 6, 2, 3];
+var min = Math.min(...myArray);
+console.log(min)
+
+
+var max = Math.max(...myArray);
+console.log(max)
+
+// check the console for the min and max values of the array
