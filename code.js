58c55d27761662f5d2da06182e56b2309771bc07
MoniqueDampies <moniquedampies1861@gmail.com>
2022-06-17 13:36:12 +0200

    updates

--- code.js
+++ code.js
@@ -1,9 +1,12 @@
-var myArray = [1, 5, 6, 2, 3];
+var myArray = [1, 5, 6, 2, 3, 4, 7];
+document.getElementById("myArray").innerHTML = myArray;
+
+
 var min = Math.min(...myArray);
-console.log(min)
+document.getElementById("min").innerHTML = min;
 
 
 var max = Math.max(...myArray);
-console.log(max)
+document.getElementById("max").innerHTML = max;
 
 // check the console for the min and max values of the array
